0bd1578338b751c16ca047c3091d42ec5fc14377
muxihong <17608507695@163.com>
2022-11-14 17:55:40 +0800

    SayHi

--- src/components/SayHi.vue
+++ src/components/SayHi.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div> SayHi </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: "SayHi"
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import SayHi from "@/components/SayHi";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "SayHi",
+      component: SayHi
     }
   ]
 })
